d06362f88f754c5826220b68f03fb247c59238c1
steppi <albert_steppi@hms.harvard.edusdfjdifjijs>
2019-03-30 17:31:22 -0400

    Register location of data files and S3 bucket

---
+++ deft_app/locations.py
@@ -0,0 +1,6 @@
+import os
+
+DEFT_APP_PATH = os.path.dirname(os.path.abspath(__file__))
+DATA_PATH = os.path.join(DEFT_APP_PATH, 'data')
+S3_BUCKET_URL = 'http://deft-models.s3.amazonaws.com'
+                                
