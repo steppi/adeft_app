6ca5b30eabbe654bf86e380d7b02f8a41931d97e
Albert Steppi <albertsteppi@Alberts-MacBook-Pro.local>
2019-04-02 10:13:14 -0400

    Purge bogus grounding with longform on click

--- deft_app/static/scripts.js
+++ deft_app/static/scripts.js
@@ -1,6 +1,5 @@
 window.onload = function() {
     groundings = document.getElementsByClassName('click-grounded');
-    longforms = document.getElementsByClassName('click-ungrounded');
     for (var i = 0; i < groundings.length; i++) {
 	groundings[i].addEventListener('click', function (event) {
 	    document.getElementById('name-box').value =
@@ -9,12 +8,4 @@ window.onload = function() {
 		event.target.getAttribute('data-grounding');
 	});
     }
-    for (var i = 0; i < longforms.length; i++) {
-	longforms[i].addEventListener('click', function (event) {
-	    document.getElementById('name-box').value =
-		event.target.getAttribute('data-longform');
-	    document.getElementById('grounding-box').value =
-		event.target.getAttribute('data-longform');
-	});
-    } 
 }
