f48b82070ac08db953a4e4bcde86062c8ba20974
Albert Steppi <albertsteppi@Alberts-MacBook-Pro.local>
2019-04-03 13:28:45 -0400

    Allow users to select which labels are positive

--- deft_app/ground.py
+++ deft_app/ground.py
@@ -2,6 +2,7 @@ import os
 import json
 import pickle
 
+
 from flask import (
     Blueprint, request, render_template, session, url_for, redirect
     )
@@ -35,9 +36,9 @@ def load_longforms():
         except ValueError:
             return render_template('index.jinja2')
     (session['longforms'], session['scores'], session['names'],
-     session['groundings']) = data
-    data = list(zip(*data))
-    return render_template('input.jinja2', data=data)
+     session['groundings'], session['pos_labels']) = data
+    data, pos_labels = _process_data(*data)
+    return render_template('input.jinja2', data=data, pos_labels=pos_labels)
 
 
 @bp.route('/input', methods=['POST'])
@@ -50,22 +51,39 @@ def add_groundings():
             index = int(value)-1
             session['names'][index] = name
             session['groundings'][index] = grounding
-    data = list(zip(session['longforms'], session['scores'],
-                    session['names'], session['groundings']))
-    return render_template('input.jinja2', data=data)
+    data, pos_labels = _process_data(session['longforms'], session['scores'],
+                                     session['names'], session['groundings'],
+                                     session['pos_labels'])
+    return render_template('input.jinja2', data=data, pos_labels=pos_labels)
 
 
 @bp.route('/delete', methods=['POST'])
 def delete_grounding():
     for key in request.form:
-        if key.startswith('delete_'):
-            id_ = key.partition('_')[-1]
+        if key.startswith('delete.'):
+            id_ = key.partition('.')[-1]
             index = int(id_) - 1
             session['names'][index] = session['groundings'][index] = ''
             break
-    data = list(zip(session['longforms'], session['scores'],
-                    session['names'], session['groundings']))
-    return render_template('input.jinja2', data=data)
+    data, pos_labels = _process_data(session['longforms'], session['scores'],
+                                     session['names'], session['groundings'],
+                                     session['pos_labels'])
+    session['pos_labels'] = pos_labels
+    return render_template('input.jinja2', data=data, pos_labels=pos_labels)
+
+
+@bp.route('/pos_label', methods=['POST'])
+def add_positive():
+    for key in request.form:
+        if key.startswith('pos-label.'):
+            label = key.partition('.')[-1]
+            session['pos_labels'] = list(set(session['pos_labels']) ^
+                                         set([label]))
+            break
+    data, pos_labels = _process_data(session['longforms'], session['scores'],
+                                     session['names'], session['groundings'],
+                                     session['pos_labels'])
+    return render_template('input.jinja2', data=data, pos_labels=pos_labels)
 
 
 @bp.route('/generate', methods=['POST'])
@@ -74,22 +92,26 @@ def generate_grounding_map():
     longforms = session['longforms']
     names = session['names']
     groundings = session['groundings']
+    pos_labels = session['pos_labels']
     grounding_map = {longform: grounding if grounding else 'ungrounded'
                      for longform, grounding in zip(longforms, groundings)}
     names_map = {grounding: name for grounding, name in zip(groundings,
                                                             names)
                  if grounding and name}
-    groundings_path = os.path.join(DATA_PATH, 'models', shortform)
+    models_path = os.path.join(DATA_PATH, 'models', shortform)
     try:
-        os.mkdir(groundings_path)
+        os.mkdir(models_path)
     except FileExistsError:
         pass
-    with open(os.path.join(groundings_path,
+    with open(os.path.join(models_path,
                            f'{shortform}_grounding_map.json'), 'w') as f:
         json.dump(grounding_map, f)
-    with open(os.path.join(groundings_path,
+    with open(os.path.join(models_path,
                            f'{shortform}_names.json'), 'w') as f:
         json.dump(names_map, f)
+    with open(os.path.join(models_path,
+                           f'{shortform}_pos_labels.json'), 'w') as f:
+        json.dump(pos_labels, f)
     return redirect(url_for('ground.main'))
 
 
@@ -100,19 +122,26 @@ def _init_with_trips(shortform, cutoff):
     names = [name if name is not None else '' for name in names]
     groundings = [grounding if grounding is not None
                   else '' for grounding in groundings]
-    return longforms, scores, names, groundings
+    labels = set(grounding for grounding in groundings if grounding)
+    pos_labels = list(set(label for label in labels
+                          if label.startswith('HGNC:') or
+                          label.startswith('FPLX:')))
+    return longforms, scores, names, groundings, pos_labels
 
 
 def _init_from_file(shortform):
     longforms, scores = _load(shortform, 0)
-    groundings_path = os.path.join(DATA_PATH, 'models', shortform)
+    models_path = os.path.join(DATA_PATH, 'models', shortform)
     try:
-        with open(os.path.join(groundings_path,
+        with open(os.path.join(models_path,
                                f'{shortform}_grounding_map.json'), 'r') as f:
             grounding_map = json.load(f)
-        with open(os.path.join(groundings_path,
+        with open(os.path.join(models_path,
                                f'{shortform}_names.json'), 'r') as f:
             names = json.load(f)
+        with open(os.path.join(models_path,
+                               f'{shortform}_pos_labels.json'), 'r') as f:
+            pos_labels = json.load(f)
     except EnvironmentError:
         raise ValueError
     groundings = [grounding_map.get(longform) for longform in longforms]
@@ -121,7 +150,7 @@ def _init_from_file(shortform):
                   if grounding is not None]
     names = [names.get(grounding) for grounding in groundings]
     names = [name if name is not None else '' for name in names]
-    return longforms, scores, names, groundings
+    return longforms, scores, names, groundings, pos_labels
 
 
 def _load(shortform, cutoff):
@@ -133,7 +162,14 @@ def _load(shortform, cutoff):
     except EnvironmentError:
         raise ValueError(f'data not currently available for shortform'
                          '{shortform}')
-    longforms, scores = zip(*[(longform, score)
+    longforms, scores = zip(*[(longform, round(score, 1))
                               for longform, score in scored_longforms
                               if score > cutoff])
     return longforms, scores
+
+
+def _process_data(longforms, scores, names, groundings, pos_labels):
+    labels = sorted(set(grounding for grounding in groundings if grounding))
+    labels.extend(['']*(len(longforms) - len(labels)))
+    data = list(zip(longforms, scores, names, groundings, labels))
+    return data, pos_labels
