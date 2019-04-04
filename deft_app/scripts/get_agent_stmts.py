06bac587744a0b9ca5da8ddb9f72cfce920533af
Albert Steppi <albertsteppi@Alberts-MacBook-Pro.local>
2019-04-04 10:12:00 -0400

    Modify get_agent_stmts to work in deft_app

--- deft_app/scripts/get_agent_stmts.py
+++ deft_app/scripts/get_agent_stmts.py
@@ -1,9 +1,22 @@
+import os
 import pickle
+import argparse
 
 from indra_db.util.content_scripts import get_stmts_with_agent_text_like
 
+from deft_app.locations import DATA_PATH
+
+
 if __name__ == '__main__':
-    two_letter = get_stmts_with_agent_text_like('ROS',
-                                                filter_genes=True)
-    with open('/deft_drive/ROS_stmts.pkl', 'wb') as f:
-        pickle.dump(two_letter, f)
+    parser = argparse.ArgumentParser(description='Get statements with agent'
+                                     'text matching a pattern')
+    parser.add_argument('pattern')
+    parser.add_argument('outfile')
+
+    args = parser.parse_args()
+    pattern = args.pattern
+    outfile = args.outfile
+    stmt_dict = get_stmts_with_agent_text_like(pattern,
+                                               filter_genes=True)
+    with open(os.path.join(DATA_PATH, 'statements', outfile), 'wb') as f:
+        pickle.dump(stmt_dict, f)
