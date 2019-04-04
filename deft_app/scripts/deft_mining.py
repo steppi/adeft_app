9d350be021a16449502a02bb4a55da3925c21f24
Albert Steppi <albertsteppi@Alberts-MacBook-Pro.local>
2019-04-04 16:26:33 -0400

    Add script to get longforms associated to a shortform

--- deft_app/scripts/deft_mining.py
+++ deft_app/scripts/deft_mining.py
@@ -1,31 +1,29 @@
 import os
 import pickle
+import argparse
 
 from deft.discover import DeftMiner
 
-data_path = os.path.join('/deft_drive', 'indra_apps', 'deft',
-                         'input')
-inpath = os.path.join(data_path, 'fulltexts')
-outpath = os.path.join(data_path, 'longforms')
+from deft_app.locations import DATA_PATH
 
 
-def mine(filepath):
-    filename = os.path.basename(filepath)
-    shortform = filename.split('_')[0]
-    with open(filepath, 'rb') as f:
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Use deft to get longforms'
+                                     ' associated with shortform')
+    parser.add_argument('shortform')
+    args = parser.parse_args()
+    shortform = args.shortform
+
+    texts_path = os.path.join(DATA_PATH, 'texts', shortform,
+                              f'{shortform}_texts.pkl')
+    with open(texts_path, 'rb') as f:
         texts = pickle.load(f)
+    texts = texts.values()
     texts = [text for text in texts if text]
     dm = DeftMiner(shortform)
     dm.process_texts(texts)
     longforms = dm.get_longforms()
-    outfile = os.path.join(outpath, f'{shortform}_longforms.pkl')
-    with open(outfile, 'wb') as f:
+    out_path = os.path.join(DATA_PATH, 'longforms',
+                            f'{shortform}_longforms.pkl')
+    with open(out_path, 'wb') as f:
         pickle.dump(longforms, f)
-
-
-if __name__ == '__main__':
-    for fl in os.listdir(inpath):
-        filename = os.fsdecode(fl)
-        if filename.endswith('.pkl'):
-            filepath = os.path.join(inpath, filename)
-            mine(filepath)
