109a73577b2fdadecf172fde50c2a092844939cb
Albert Steppi <albertsteppi@Alberts-MacBook-Pro.local>
2019-04-16 17:09:25 -0400

    Finish implementation of model statistics

--- deft_app/scripts/model.py
+++ deft_app/scripts/model.py
@@ -3,7 +3,11 @@ import json
 import pickle
 import argparse
 import pandas as pd
-from collections import defaultdict
+from collections import Counter, defaultdict
+
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import cross_val_predict
+
 
 from deft.recognize import DeftRecognizer
 from deft.modeling.classify import DeftClassifier
@@ -13,6 +17,10 @@ from deft_app.locations import DATA_PATH
 
 
 def train(shortforms, additional=None, n_jobs=1):
+    """Train a deft model and produce quality statistics"""
+    if additional is None:
+        additional = []
+    # gather needed data
     groundings_path = os.path.join(DATA_PATH, 'groundings')
     texts_path = os.path.join(DATA_PATH, 'texts')
     models_path = os.path.join(DATA_PATH, 'models')
@@ -20,6 +28,7 @@ def train(shortforms, additional=None, n_jobs=1):
     grounding_dict = {}
     names = {}
     pos_labels = set()
+    # combine grounding maps and names from multiple shortforms into one model
     for shortform in shortforms:
         with open(os.path.join(groundings_path, shortform,
                                f'{shortform}_grounding_map.json'), 'r') as f:
@@ -33,6 +42,8 @@ def train(shortforms, additional=None, n_jobs=1):
             pos_labels.update(json.load(f))
     pos_labels = sorted(pos_labels)
 
+    # model name is built up from shortforms in model
+    # (most models only have one shortform)
     agg_name = ':'.join(sorted(shortforms))
     with open(os.path.join(texts_path, agg_name,
                            f'{agg_name}_texts.json'), 'r') as f:
@@ -41,20 +52,60 @@ def train(shortforms, additional=None, n_jobs=1):
                            f'{agg_name}_text_map.json'), 'r') as f:
         ref_dict = json.load(f)
 
+    # get statistics for matches to standard patterns
     stats = deft_stats(grounding_dict, names, text_dict, ref_dict)
-    return stats
 
+    # build corpus for training models
     refs, texts = zip(*text_dict.items())
     texts = [text for text in texts if text is not None]
     deft_cb = DeftCorpusBuilder(grounding_dict)
     corpus = deft_cb.build_from_texts(texts)
+
+    # gather additional texts
+    for grounding, name, agent_text in additional:
+        names[grounding] = name
+        with open(os.path.join(texts_path, agent_text,
+                               f'{agent_text}_texts.json'), 'r') as f:
+            additional_texts = json.load(f)
+            corpus.append([(text, grounding)
+                           for text_ref, text in additional_texts.items()
+                           if text_ref not in text_dict])
 
     train, labels = zip(*corpus)
     deft_cl = DeftClassifier(shortforms, pos_labels)
     params = {'C': [100.0], 'max_features': [10000],
               'ngram_range': [(1, 2)]}
     deft_cl.cv(train, labels, params, n_jobs=n_jobs, cv=5)
+    cv = deft_cl.grid_search.cv_results_
 
+    preds = cross_val_predict(deft_cl.estimator, train, labels, n_jobs=n_jobs,
+                              cv=5)
+    conf_matrix = confusion_matrix(labels, preds)
+    cv_results = {'labels': sorted(set(labels)),
+                  'conf_matrix': conf_matrix.tolist(),
+                  'f1': {'mean': cv['mean_test_f1'][0],
+                         'std': cv['std_test_f1'][0]},
+                  'precision': {'mean': cv['mean_test_pr'][0],
+                                'std': cv['std_test_pr'][0]},
+                  'recall': {'mean': cv['mean_test_rc'][0],
+                             'std': cv['std_test_rc'][0]}}
+
+    unlabeled = []
+    recognizers = [DeftRecognizer(shortform, grounding_map)
+                   for shortform, grounding_Map in grounding_dict.items()]
+    for text in texts:
+        for rec in recognizers:
+            if rec.recognize(text):
+                break
+        else:
+            unlabeled.append(text)
+
+    preds = deft_cl.estimator.predict(texts)
+    preds = dict(Counter(preds))
+    data = {'stats': stats,
+            'cv_results': cv_results,
+            'preds_on_unlabeled': preds}
+  
     try:
         os.mkdir(os.path.join(models_path, agg_name))
     except FileExistsError:
@@ -67,24 +118,37 @@ def train(shortforms, additional=None, n_jobs=1):
     with open(os.path.join(models_path, agg_name,
                            f'{agg_name}_names.json'), 'w') as f:
         json.dump(names, f)
-    return deft_cl, stats
+    with open(os.path.join(models_path, agg_name,
+                           f'{agg_name}_stats.json'), 'w') as f:
+        json.dump(data, f)
+    return deft_cl
 
 
 def deft_stats(grounding_dict, names_dict, text_dict, ref_dict):
+    """Output deft pattern matching stats as dict that can jsonified"""
+    # need to run each recognizer on every text
     recognizers = [DeftRecognizer(shortform, grounding_map)
                    for shortform, grounding_map in grounding_dict.items()]
 
+    # given dict mapping stmt ids to text_ref ids, get dict mapping
+    # text_ref ids to counts of stmts from those texts
     stmt_counts = defaultdict(int)
     for stmt, ref in ref_dict.items():
         stmt_counts[ref] += 1
     stmt_counts = dict(stmt_counts)
+
+    # get all groundings from grounding_dict
     groundings = {value for grounding_map in grounding_dict.values()
                   for value in grounding_map.values()}
+    # get stats in pandas dataframe. columns for groundings. 1 if
+    # text has this grounding, 0 if it doesn't. Also stmt count for
+    # each text_ref
+    # start with row template as dict. build up row by row
     row_template = {grounding: 0
                     for grounding in groundings}
     row_template['text_ref'] = None
     row_template['num_stmts'] = 0
-    out = []
+    df = []
     for ref, text in text_dict.items():
         row = row_template.copy()
         row['text_ref'] = ref
@@ -93,34 +157,42 @@ def deft_stats(grounding_dict, names_dict, text_dict, ref_dict):
             ground = recognizer.recognize(text)
             for g in ground:
                 row[g] = 1
-        out.append(row)
-    df = pd.DataFrame(out)
+        df.append(row)
+    df = pd.DataFrame(df)
+
+    # rearrange columns so text_ref is first and num_stmts is
+    # last
     cols = df.columns.tolist()
     ref_col = cols.index('text_ref')
     cols = [cols[ref_col]] + cols[0:ref_col] + cols[ref_col+1:]
     nstmts_col = cols.index('num_stmts')
-    cols = cols[0:nstmts_col] + cols[nstmts_col+1:] + [cols[nstmts_col]]
+    cols = (cols[0:nstmts_col] + cols[nstmts_col+1:] +
+            [cols[nstmts_col]])
     df = df[cols]
 
+    output = {}
+    # get shortforms for json
+    output['shortforms'] = list(grounding_dict.keys())
+
+    # get total number of statements and total texts
     nstmts = df.num_stmts.sum()
     ntexts = len(df)
-    shortforms = list(grounding_dict.keys())
-    shortforms = ' '.join(shortforms)
-    out = f'For shortform {shortforms} there are:\n'
-    out += f'{nstmts} statements from {ntexts} texts.\n'
+    output['total'] = {'stmts': int(nstmts), 'texts': int(ntexts)}
 
+    # get number of texts and matching defining pattern for
+    # one of the shortforms and number of corresponding statements
     ground_cols = df.drop(['text_ref', 'num_stmts'], axis=1)
     matched = ground_cols.any(axis=1)
-    out += (f'Of these texts, {matched.sum()} contain a defining pattern for'
-            ' this shortform.\n')
-    out += f'These account for {df[matched].num_stmts.sum()} statements.\n'
-    unique = ground_cols[~(ground_cols.sum(axis=1) > 1)]
+
+    output['match_pattern'] = {'stmts': int(df[matched].num_stmts.sum()),
+                               'texts': int(matched.sum())}
+    output['groundings'] = {}
+    unique = df[~(ground_cols.sum(axis=1) > 1)]
     for column in unique.columns:
-        s = unique[column].sum()
-        if column != 'ungrounded':
-            out += (f'{s} texts can unambiguously be assigned grounding'
-                    f' {column} with name {names_dict[column]}.\n')
-        else:
-            out += f'{s} texts have a pattern matching an ungrounded term.\n'
-    print(out)
-    return df
+        if column in ['text_ref', 'num_stmts']:
+            continue
+        ntexts = unique[column].sum()
+        nstmts = unique[unique[column] == 1]['num_stmts'].sum()
+        output['groundings'][column] = {'stmts': int(nstmts),
+                                        'texts': int(ntexts)}
+    return output
