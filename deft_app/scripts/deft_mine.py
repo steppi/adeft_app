eaac5e85c3af23ee5d91597a82efa419e172dc5b
Albert Steppi <albertsteppi@Alberts-MacBook-Pro.local>
2019-04-16 17:16:56 -0400

    Handle deft mining with synonymous shortforms

--- deft_app/scripts/deft_mine.py
+++ deft_app/scripts/deft_mine.py
@@ -10,19 +10,21 @@ from deft_app.locations import DATA_PATH
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Use deft to get longforms'
                                      ' associated with shortform')
-    parser.add_argument('shortform')
+    parser.add_argument('vars', nargs='*')
     args = parser.parse_args()
-    shortform = args.shortform
-    texts_path = os.path.join(DATA_PATH, 'texts', shortform,
-                              f'{shortform}_texts.json')
+    shortforms = args.vars
+    agg_name = ':'.join(sorted(shortforms))
+    texts_path = os.path.join(DATA_PATH, 'texts', agg_name,
+                              f'{agg_name}_texts.json')
     with open(texts_path, 'r') as f:
         texts = json.load(f)
     texts = texts.values()
     texts = [text for text in texts if text]
-    dm = DeftMiner(shortform)
-    dm.process_texts(texts)
-    longforms = dm.get_longforms()
-    out_path = os.path.join(DATA_PATH, 'longforms',
-                            f'{shortform}_longforms.json')
-    with open(out_path, 'w') as f:
-        json.dump(longforms, f)
+    for shortform in shortforms:
+        dm = DeftMiner(shortform)
+        dm.process_texts(texts)
+        longforms = dm.get_longforms()
+        out_path = os.path.join(DATA_PATH, 'longforms',
+                                f'{shortform}_longforms.json')
+        with open(out_path, 'w') as f:
+            json.dump(longforms, f)
