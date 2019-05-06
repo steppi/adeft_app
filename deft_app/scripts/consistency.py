036babd644bbdba1d763ead68224315dd93cc08d
Albert Steppi <albert_steppi@hms.harvard.edu>
2019-05-06 12:36:39 -0400

    Add functions for checking modeling consistency

--- deft_app/scripts/consistency.py
+++ deft_app/scripts/consistency.py
@@ -10,11 +10,64 @@ from deft_app.locations import DATA_PATH
 logger = logging.getLogger(__file__)
 
 
+def check_grounding_dict(grounding_dict):
+    """Check that a grounding_dict doesn't have the same longform mapping
+    to different groundings in different grounding maps.
+    """
+    return check_dictionaries(grounding_dict.values())
+
+
+def check_consistency_names_grounding_dict(grounding_dict, names_map):
+    """Check that a grounding dict and names map have consistent names
+    """
+    groundings = {grounding for grounding_map in grounding_dict.values()
+                  for grounding in grounding_map.values()
+                  if grounding != 'ungrounded'}
+    return groundings == set(names_map.keys())
+
+
+def check_consistency_grounding_dict_pos_labels(grounding_dict, pos_labels):
+    """Check that there are no pos labels not in the grounding dict
+    """
+    groundings = {grounding for grounding_map in grounding_dict.values()
+                  for grounding in grounding_map.values()
+                  if grounding != 'ungrounded'}
+    return set(pos_labels) <= groundings
+
+
+def check_model_consistency(model, grounding_dict, pos_labels):
+    """Check that serialized model is consistent with associated json files.
+    """
+    groundings = {grounding for grounding_map in grounding_dict.values()
+                  for grounding in grounding_map.values()}
+    model_labels = set(model.estimator.named_steps['logit'].classes_)
+    consistent_labels = groundings == model_labels
+
+    shortforms = set(grounding_dict.keys())
+    model_shortforms = set(model.shortforms)
+    consistent_shortforms = shortforms == model_shortforms
+
+    pos_labels = set(pos_labels)
+    model_pos_labels = set(model.pos_labels)
+    consistent_pos_labels = pos_labels == model_pos_labels
+
+    model_labels = set(model.estimator.named_steps['logit'].classes_)
+
+    return consistent_labels and consistent_shortforms and \
+        consistent_pos_labels
+
+
+def check_names_consistency(names_list):
+    """Ensure names maps are consistent for model with multiple shortforms
+    """
+    return check_dictionaries(names_list)
+
+
 def check_dictionaries(dicts):
     """Check if a list of dictionaries are pairwise consistent
 
-    Two dictionaries are consistent if there does not exist a key k that
-    has a different associated value in each dictionary.
+    Two dictionaries are consistent with eachother if there does not exist a
+    key k that has a different associated value in each dictionary
     """
     big_dict = defaultdict(list)
     for dictionary in dicts:
@@ -23,51 +76,6 @@ def check_dictionaries(dicts):
     lengths = [len(value) for value in big_dict.values]
     return max(lengths) == 1
 
-
-def check_consistency(model_name):
-    """This checks if all model files are consistent with each other.
-
-    This is horribly written and will be refactored soon. Just trying to
-    get it working for now.
-    """
-    model_path = os.path.join(DATA_PATH, 'models', model_name)
-    with open(os.path.join(model_path,
-                           f'{model_name}_grounding_dict.json'), 'r') as f:
-        grounding_dict = json.load(f)
-    with open(os.path.join(model_path,
-                           f'{model_name}_names.json'), 'r') as f:
-        model_names = json.load(f)
-        groundings = {grounding for grounding_map in grounding_dict.values()
-                      for grounding in grounding_map.values()
-                      if grounding != 'ungrounded'}
-        if groundings != set(model_names.keys()):
-            logger.warning('inconsistent groundings in names map and'
-                           ' grounding maps for model.')
-            return False
-    with open(os.path.join(model_path,
-                           f'{model_name}_pos_labels.json'), 'r') as f:
-        pos_labels = json.load(f)
-    if not (set(pos_labels) <= groundings):
-        logger.warning('positive labels exist that are not in list of'
-                       ' groundings')
-        return False
-    model = load_model(os.path.join(model_path, f'{model_name}_model.gz'))
-    classes = model.estimator.named_steps['logit'].classes_
-    if sorted(pos_labels) != sorted(classes):
-        logger.warning('labels for deft classifier out of sync with labels'
-                       ' in file.')
-    names_list = []
-    local_pos_labels = set()
-    for shortform, gmap1 in grounding_dict:
-        with open(os.path.join(DATA_PATH, 'groundings', shortform,
-                               f'{shortform}_grounding_map.json'), 'r') as f:
-            gmap2 = json.load(f)
-        if gmap1 != gmap2:
-            logger.warning('inconsistent grounding maps for model.')
-            return False
-        with open(os.path.join(DATA_PATH, 'groundings', shortform,
-                               f'{shortform}_names.json'), 'r') as f:
-            names_list.append(json.load(f))
         with open(os.path.join(DATA_PATH, 'groundings', shortform,
                                f'{shortform}_pos_labels.json'), 'r') as f:
             local_pos_labels.update(json.load(f))
