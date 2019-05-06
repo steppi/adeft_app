febacaacc513aea6681116fef18568e410447f9e
Albert Steppi <albert_steppi@hms.harvard.edu>
2019-05-06 16:37:11 -0400

    Use new period free bucket name

--- deft_app/locations.py
+++ deft_app/locations.py
@@ -2,4 +2,4 @@ import os
 
 DEFT_APP_PATH = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(DEFT_APP_PATH, 'data')
-S3_BUCKET = 'deft-models-0.2.0'
+S3_BUCKET = 'deft-models-0-2-0'
