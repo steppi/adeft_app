93b3c968e7aa54d675744ff8fee8ee11eafe90f9
Albert Steppi <albert_steppi@hms.harvard.edu>
2019-05-08 14:24:36 -0400

    Split grounding and fixes into separate files

--- deft_app/ground.py
+++ deft_app/ground.py
@@ -1,37 +1,24 @@
 import os
 import json
 import logging
-from copy import deepcopy
-from collections import defaultdict
 
 from flask import (
     Blueprint, request, render_template, session, url_for, redirect
     )
 
-from deft.modeling.classify import load_model
 
 from .trips import trips_ground
 from .locations import DATA_PATH
-from .scripts.model_to_s3 import model_to_s3
-from .scripts.consistency import (check_grounding_dict,
-                                  check_model_consistency,
-                                  check_consistency_grounding_dict_pos_labels,
-                                  check_consistency_names_grounding_dict,
-                                  check_names_consistency)
 
 
 logger = logging.getLogger(__file__)
 
+
 bp = Blueprint('ground', __name__)
 
 
-@bp.route('/')
-def main():
-    return render_template('index.jinja2')
-
-
-@bp.route('/longforms', methods=['POST'])
-def load_longforms():
+@bp.route('/ground_init', methods=['POST'])
+def initialize():
     shortform = request.form['shortform']
     session['shortform'] = shortform
     try:
@@ -51,172 +38,7 @@ def load_longforms():
     return render_template('input.jinja2', data=data, pos_labels=pos_labels)
 
 
-@bp.route('/fix', methods=['POST'])
-def fix():
-    model_name = request.form['modelname']
-    if not model_name:
-        return render_template('index.jinja2')
-    models_path = os.path.join(DATA_PATH, 'models', model_name)
-    with open(os.path.join(models_path,
-                           model_name + '_grounding_dict.json')) as f:
-        grounding_dict = json.load(f)
-    with open(os.path.join(models_path, model_name + '_names.json')) as f:
-        names = json.load(f)
-    longforms = defaultdict(list)
-    longform_scores = defaultdict(int)
-    for shortform, grounding_map in grounding_dict.items():
-        with open(os.path.join(DATA_PATH, 'longforms',
-                               f'{shortform}_longforms.json'), 'r') as f:
-            lf_scores = json.load(f)
-            for lf, score in lf_scores:
-                longform_scores[lf] += score
-        for longform, grounding in grounding_map.items():
-            if grounding != 'ungrounded':
-                longforms[grounding].append(longform)
-    top_longforms = {grounding: max(longform_list,
-                                    key=lambda x: longform_scores[x])
-                     for grounding, longform_list in longforms.items()}
-    longforms = [[grounding, '\n'.join(longform)] for grounding, longform
-                 in longforms.items()]
-    original_longforms = deepcopy(longforms)
-    transition = {grounding: grounding for grounding, _ in longforms}
-    transition['ungrounded'] = 'ungrounded'
-    session['transition'] = transition
-    session['model_name'] = model_name
-    session['longforms'], session['names'] = longforms, names
-    session['top_longforms'] = top_longforms
-    session['original_longforms'] = original_longforms
-    return render_template('fix.jinja2', longforms=longforms, names=names,
-                           top_longforms=top_longforms)
-
-
-@bp.route('/fix_groundings', methods=['POST'])
-def fix_groundings():
-    for key in request.form:
-        if key.startswith('s.'):
-            index = key.partition('.')[-1]
-            new_name = request.form[f'new-name.{index}']
-            new_ground = request.form[f'new-ground.{index}']
-            names = session['names']
-            longforms = session['longforms']
-            original_longforms = session['original_longforms']
-            old_ground = longforms[int(index)-1][0]
-            origin_ground = original_longforms[int(index)-1][0]
-            if new_name:
-                names[old_ground] = new_name
-            if new_ground:
-                longforms[int(index)-1][0] = new_ground
-                names[new_ground] = names.pop(old_ground)
-                transition = session['transition']
-                transition[origin_ground] = new_ground
-                top_longforms = session['top_longforms']
-                top_longforms[new_ground] = top_longforms.pop(old_ground)
-                session['top_longforms'] = top_longforms
-            session['longforms'], session['names'] = longforms, names
-    return render_template('fix.jinja2', longforms=session['longforms'],
-                           names=session['names'],
-                           top_longforms=session['top_longforms'])
-
-
-@bp.route('/submit_fix', methods=['POST'])
-def submit_fix():
-    model_name = session['model_name']
-    models_path = os.path.join(DATA_PATH, 'models', model_name)
-    with open(os.path.join(models_path,
-                           f'{model_name}_grounding_dict.json')) as f:
-        grounding_dict = json.load(f)
-    model = load_model(os.path.join(models_path,
-                                    f'{model_name}_model.gz'))
-    transition = session['transition']
-    names = session['names']
-    grounding_dict = {shortform: {longform: transition[grounding]
-                                  for longform, grounding in
-                                  grounding_map.items()}
-                      for shortform, grounding_map in grounding_dict.items()}
-
-    if not check_grounding_dict(grounding_dict):
-        logger.error('grounding dict has become inconsistent.\n'
-                     'This should not happen if the program is working'
-                     ' as expected.')
-        return redirect(url_for('submit_fix'))
-
-    for index, label in enumerate(model.estimator.classes_):
-        model.estimator.classes_[index] = transition[label]
-
-    with open(os.path.join(models_path,
-                           f'{model_name}_pos_labels.json'), 'r') as f:
-        pos_labels = json.load(f)
-    pos_labels = sorted(transition[label] for label in pos_labels)
-
-    if not check_consistency_grounding_dict_pos_labels(grounding_dict,
-                                                       pos_labels):
-        logger.error('pos labels exist that are not in grounding dict')
-        return redirect(url_for('submit_fix'))
-
-    if not check_consistency_names_grounding_dict(grounding_dict, names):
-        logger.error('names have become out of sync with grounding dict.')
-        return redirect(url_for('submit_fix'))
-
-    if not check_model_consistency(model, grounding_dict, pos_labels):
-        logger.error('Model state has become inconsistent.')
-        return redirect(url_for('submit_fix'))
-
-    # update groundings files created before training model
-    groundings_path = os.path.join(DATA_PATH, 'groundings')
-    names_dict = {}
-    pos_labels_dict = {}
-    for shortform, grounding_map in grounding_dict.items():
-        with open(os.path.join(groundings_path, shortform,
-                               f'{shortform}_names.json'), 'r') as f:
-            temp = json.load(f)
-        names_dict[shortform] = {transition[label]: name
-                                 for label, name in temp.items()}
-        with open(os.path.join(groundings_path, shortform,
-                               f'{shortform}_pos_labels.json'), 'r') as f:
-            temp = json.load(f)
-        pos_labels_dict[shortform] = [transition[label]
-                                      for label in pos_labels]
-
-    if not check_names_consistency(names_dict.values()):
-        logger.error('Inconsistent names for equivalent shortforms.')
-        return redirect(url_for('submit_fix'))
-
-    all_pos_labels = sorted(pos_label for labels in pos_labels_dict.values()
-                            for pos_label in labels)
-
-    if not all_pos_labels == pos_labels:
-        logger.error('positive labels have become out of sync for model'
-                     ' and groundings files.')
-        return redirect(url_for('submit_fix'))
-
-    # update files for model
-    with open(os.path.join(models_path,
-                           f'{model_name}_grounding_dict.json'), 'w') as f:
-        json.dump(grounding_dict, f)
-    with open(os.path.join(models_path,
-                           f'{model_name}_names.json'), 'w') as f:
-        json.dump(names, f)
-    with open(os.path.join(models_path,
-                           f'{model_name}_pos_labels.json'), 'w') as f:
-        json.dump(pos_labels, f)
-    model.dump_model(os.path.join(models_path, f'{model_name}_model.gz'))
-
-    # update groundings files used for training model
-    for shortform, grounding_map in grounding_dict.items():
-        with open(os.path.join(groundings_path, shortform,
-                               f'{shortform}_grounding_map.json'), 'w') as f:
-            json.dump(grounding_map, f)
-        with open(os.path.join(groundings_path, shortform,
-                               f'{shortform}_names.json'), 'w') as f:
-            json.dump(names_dict[shortform], f)
-        with open(os.path.join(groundings_path, shortform,
-                               f'{shortform}_pos_labels.json'), 'w') as f:
-            json.dump(pos_labels_dict[shortform], f)
-    session.clear()
-    return render_template('index.jinja2')
-
-
-@bp.route('/input', methods=['POST'])
+@bp.route('/ground_add', methods=['POST'])
 def add_groundings():
     name = request.form['name']
     grounding = request.form['grounding']
@@ -235,7 +57,7 @@ def add_groundings():
     return render_template('input.jinja2', data=data, pos_labels=pos_labels)
 
 
-@bp.route('/delete', methods=['POST'])
+@bp.route('/ground_delete', methods=['POST'])
 def delete_grounding():
     names, groundings = session['names'], session['groundings']
     for key in request.form:
@@ -253,7 +75,7 @@ def delete_grounding():
     return render_template('input.jinja2', data=data, pos_labels=pos_labels)
 
 
-@bp.route('/pos_label', methods=['POST'])
+@bp.route('/ground_pos_label', methods=['POST'])
 def add_positive():
     for key in request.form:
         if key.startswith('pos-label.'):
@@ -267,7 +89,7 @@ def add_positive():
     return render_template('input.jinja2', data=data, pos_labels=pos_labels)
 
 
-@bp.route('/generate', methods=['POST'])
+@bp.route('/ground_generate', methods=['POST'])
 def generate_grounding_map():
     shortform = session['shortform']
     longforms = session['longforms']
