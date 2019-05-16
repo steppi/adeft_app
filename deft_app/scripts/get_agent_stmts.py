443f037904226001c9c147c1a58e4fe8da3b0aa9
Albert Steppi <albert_steppi@hms.harvard.edu>
2019-05-16 13:32:27 -0400

    Escape lower case characters in filename

--- deft_app/scripts/get_agent_stmts.py
+++ deft_app/scripts/get_agent_stmts.py
@@ -7,7 +7,7 @@ import argparse
 from indra_db.util.content_scripts import get_stmts_with_agent_text_like
 
 from deft_app.locations import DATA_PATH
-
+from deft_app.file_names import escape_lower_case
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Get statements with agent'
@@ -24,7 +24,10 @@ if __name__ == '__main__':
     stmt_dict = get_stmts_with_agent_text_like(pattern,
                                                filter_genes=True)
     for shortform, stmts in stmt_dict.items():
+        cased_shortform = escape_lower_case(shortform)
         if re.match(keep, shortform):
-            with open(os.path.join(DATA_PATH, 'statements',
-                                   f'{shortform}_statements.json'), 'w') as f:
+            with open(os.path.join(DATA_PATH,
+                                   'statements',
+                                   f'{cased_shortform}_statements.json'),
+                      'w') as f:
                 json.dump(stmts, f)
